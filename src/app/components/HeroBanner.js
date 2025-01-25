441278af989481e80c100d87fa7c417a5241c2b0
Ajeet Singh <2016appsingh@gmail.com>
2025-01-25 21:48:11 +0530

    resume link changed

--- src/app/components/HeroBanner.js
+++ src/app/components/HeroBanner.js
@@ -55,7 +55,7 @@ export default function HeroBanner() {
           <span className="text-green-500 font-semibold">Mern Stack</span>
         </div>
         <a
-          href="https://ncert-epub.s3.us-east-1.amazonaws.com/test/Ajeet's+Resume.pdf"
+          href="https://ncert-epub.s3.us-east-1.amazonaws.com/test/Ajeet+Resume.pdf"
           download
           className="px-8 py-3 bg-gradient-to-r from-green-400 to-blue-500 text-white rounded-xl shadow-lg transform hover:scale-105 transition duration-300"
         >
