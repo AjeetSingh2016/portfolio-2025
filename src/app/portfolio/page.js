3db9490548a0515b03969bf14f983c1d2195e4b6
Ajeet Singh <2016appsingh@gmail.com>
2026-02-08 00:44:48 +0530

    route changed

---
+++ src/app/portfolio/page.js
@@ -0,0 +1,25 @@
+import Header from "../components/Header";
+import HeroBanner from "../components/HeroBanner";
+import AboutMe from "../components/AboutMe";
+import Portfolio from "../components/Portfolio";
+import Blogs from "../components/Blogs";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
+import Services from "../components/Services";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <HeroBanner />
+        <AboutMe />
+        <Services />
+        <Portfolio />
+        <Blogs />
+        <ContactForm />
+      </main>
+      <Footer/>
+    </>
+  );
+}
